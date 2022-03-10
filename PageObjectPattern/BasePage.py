aa7af07ef10bed2b189b9fd4057a92d4d7b68bb0
jongsungbae <jongsung.jaybae@gmail.com>
2022-03-10 12:59:42 -0500

    create Base page

---
+++ PageObjectPattern/BasePage.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod
+
+class BasePage(object):
+    def __init__(self, driver):
+        self._validate_page(driver)
+        self.driver = driver
+
+    @abstractmethod
+    def _validate_page(self, driver):
+        return
+
+    @property
+    def search(self):
+        from search import SearchRegion
+        return SearchRegion(self.driver)
+
+class InvalidPageException(Exception):
+    pass
+
+
