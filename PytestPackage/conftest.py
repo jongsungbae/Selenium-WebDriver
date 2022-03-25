e84042095912d1fbe647f4125bdc82ba5101d17c
jongsungbae <jongsung.jaybae@gmail.com>
2022-03-25 17:24:37 -0400

    command line args

--- PytestPackage/conftest.py
+++ PytestPackage/conftest.py
@@ -7,7 +7,23 @@ def setUp():
     print("Running method level tearDown")
 
 @pytest.fixture(scope="module")
-def oneTimeSetUp():
+def oneTimeSetUp(browser, osType):
     print("Running one time setUp")
+    if browser == 'Chrome':
+        print("Runnign test on the Chrome")
+    else:
+        print("Running test on FF")
     yield
     print("Running one time tearDown")
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--osType", help="Type of operating system")
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("--browser")
+
+@pytest.fixture(scope="session")
+def osType(request):
+    return request.config.getoption("--osType")
---
+++ PytestPackage/test_command_line_args.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_command_line_methodA(oneTimeSetUp, setUp):
+    print("Running method A")
+
+def test_command_line_methodB(oneTimeSetUp, setUp):
+    print("Running method B")
+
+
+
